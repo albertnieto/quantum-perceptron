0c1419135c026ed9e8c92bdfa0f357036f249ca6
Albert <adalbert123640@gmail.com>
2024-05-19 20:38:25 +0200

    Create perceptron_albert.py

---
+++ src/model/perceptron_albert.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Perceptron_Albert(object):
+
+    def __init__(self, n_iter=1):
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.w = np.zeros(n_features, dtype=np.float64)
+        self.b = 0.0
+
+        for t in range(self.n_iter):
+            for i in range(n_samples):
+                if self.predict(X[i])[0] != y[i]:
+                    self.w += y[i] * X[i]
+                    self.b += y[i]
+
+    def project(self, X):
+        return np.dot(X, self.w) + self.b
+
+    def predict(self, X):
+        X = np.atleast_2d(X)
+        return np.sign(self.project(X))
